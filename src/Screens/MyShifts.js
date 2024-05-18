bef6521cbb5374e855fbb316a1f071168056a1db
Nandini panchal <nandini_panchal@apprely.com>
2024-05-18 10:41:24 +0530

    my shifts added

--- src/Screens/MyShifts.js
+++ src/Screens/MyShifts.js
@@ -1,21 +1,96 @@
-import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
+import _ from 'lodash';
+import moment from 'moment';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
 import * as shifts from '../services/shifts';
 
 const MyShifts = () => {
+  const [myShifts, setMyShifts] = useState([]);
   useEffect(() => {
     const getAllShifts = async () => {
       const res = await shifts.getAllShifts();
-      console.log('res===', res);
+      let newShifts = res.map(item => {
+        return {
+          ...item,
+          day: moment(item.startTime).format('MMM DD'),
+          startTime: moment(item.startTime).format('H:mm'),
+          endTime: moment(item.endTime).format('H:mm'),
+        };
+      });
+      newShifts = _.groupBy(newShifts, 'day');
+      setMyShifts(newShifts);
     };
     getAllShifts();
   }, []);
 
+  const displayMyShifts = item => {
+    console.log('item shifts', item);
+    const shifts = item.item;
+    return (
+      <View style={styles.flatListContainer}>
+        <View style={styles.item}>
+          <View>
+            <Text>
+              {shifts.startTime}-{shifts.endTime}
+            </Text>
+            <Text>{shifts.area}</Text>
+          </View>
+          <View style={styles.cancelBtn}>
+            <Button title="Cancel" />
+          </View>
+        </View>
+      </View>
+    );
+  };
   return (
-    <View>
-      <Text>My shifts!</Text>
-    </View>
+    <ScrollView>
+      <View>
+        {Object.keys(myShifts).map(key => {
+          return (
+            <View>
+              <View>
+                <Text style={styles.title}>{key}</Text>
+              </View>
+              <FlatList
+                keyExtractor={item => item.id}
+                data={myShifts[key]}
+                renderItem={item => displayMyShifts(item)}
+              />
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
 
 export default MyShifts;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  flatListContainer: {
+    marginBottom: 20,
+  },
+
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    margin: 2,
+  },
+  cancelBtn: {marginLeft: 'auto'},
+});
